3c732c20647fe0d60796d40e66e41c76e86d7028
ZIV <solderz@yandex.ru>
2023-08-30 11:35:02 +0300

    proverka sliyanie_1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print('Новая запись в вслияение')
-print('Вторая строчка в слияние')
+print('Новая зadsasdапись в вслияение')
+print('Втораasdя строчка в слияние')
 print('ahorosho')
 print('Tupsson!dasd')
 print()
